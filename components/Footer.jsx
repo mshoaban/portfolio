8a861bcf50f984a02485451134fd77adc850749a
muhammad-shoaban <mshoaban234@gmail.com>
2026-06-26 14:40:17 +0500

    footer fix

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,10 +2,8 @@
 
 export default function Footer() {
   return (
-    <footer className="border-t border-gray-200 dark:border-gray-800 mt-24">
         <div className="mt-10 pt-6 border-t border-gray-200 dark:border-gray-800 text-center text-sm text-gray-500">
           © {new Date().getFullYear()} Muhammad Shoaban. All rights reserved.
         </div>
-    </footer>
   );
 }
