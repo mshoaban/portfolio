5fd4f4c6ab74bbbfe7a445a16be5dd4131cd123d
muhammad-shoaban <mshoaban234@gmail.com>
2026-09-07 07:51:35 +0500

    speed insights

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import PortfolioHome from "@/components/PortfolioHome";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
     <>
       <PortfolioHome />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
