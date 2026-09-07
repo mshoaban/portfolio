9da53dddcbf83a6fe15ef8009020a258b09d0c12
muhammad-shoaban <mshoaban234@gmail.com>
2026-09-07 07:43:45 +0500

    analytics

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,13 @@
 import PortfolioHome from "@/components/PortfolioHome";
+import { Analytics } from '@vercel/analytics/next';
+
 
 export default function Home() {
-  return <PortfolioHome />;
+  // return <PortfolioHome />;
+  return (
+    <>
+      <PortfolioHome />
+      <Analytics />
+    </>
+  );
 }
